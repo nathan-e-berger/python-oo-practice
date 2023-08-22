803086a630e2fe4da662e8a550c70bb076b3a2ad
Nathan Berger <nberger101@gmail.com>
2023-08-22 18:34:35 -0400

    added serialgenerator's methods and passed initial tests

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,20 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create generator instance from included start value"""
+        self.start = start
+        self.count = start - 1
+
+
+    def __repr__(self):
+        return f"<SerialGenerator starts at {self.start}>"
+
+    def generate(self):
+        """Increment from the start value"""
+        self.count = self.count + 1
+        return self.count
+
+    def reset(self):
+        """Reset the count back to initial start value"""
+        self.count = self.start - 1
